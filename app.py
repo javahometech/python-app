bb0904917ce2e1cb4815925ab7b8398a0d5aa9bd
javahometech <kammana.hari@gmail.com>
2018-09-18 10:16:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # count = redis.incr('hits')
-    return 'Hello World! Im running in a docker Container - New Features Added'
+    return 'Hello World! Im running in a docker Container - Version 0.0.2'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
