f1385d1c34166026aef3267259a3ed0289e560f3
javahometech <hari.kammana@gmail.com>
2023-03-17 18:10:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask
-from redis import Redis
 import os
 
 app = Flask(__name__)
 
-redis = Redis(host='redis', port=6379)
-
 @app.route('/')
 def hello():
-    count = redis.incr('hits')
-    return '<h1 style="color:red">Welcome to Java Home python app - number of hits to redis -> {}</h1>'.format(count)
+    return '<h1 style="color:red">Welcome to Java Home Docker App </h1>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
