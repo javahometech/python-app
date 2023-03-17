b264c814a0d83ba3211fae5a12cdb058c029105a
javahometech <hari.kammana@gmail.com>
2023-03-17 18:54:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1 style="color:green">Welcome to Java Home Docker App </h1>'
+    return '<h1 style="color:orange">Welcome to Java Home Docker App </h1>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
